a2e9630608228b8646206d1a9a1cf34035a2f387
Rafael William <rafael.fim@hotmail.com>
2023-05-29 16:10:17 -0300

    custom font and loading component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,28 @@
+import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
 import { SignIn } from './src/screens/SignIn';
 import theme from './src/theme';
+import { Loading } from './src/components/Loading';
 
 export function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
+
+  // enquanto a fonte não for carregada
+  if (!fontsLoaded) {
+    return (
+      <Loading />
+    )
+  }
+
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
       <SignIn />
     </ThemeProvider>
   );
